4d800c7f0bf5e8c983092e23e6342be34a7e7753
cskmacedo <kleber.barreto@msn.com>
2022-10-18 13:01:59 -0300

    Adicionado teste

---
+++ src/main/java/com/lambdateam/mycar/MycarApplication.java
@@ -0,0 +1,13 @@
+package com.lambdateam.mycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambdateam/mycar/MycarApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lambdateam.mycar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MycarApplicationTests {
+
+    @Test
+    void testOperation() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+
+}
