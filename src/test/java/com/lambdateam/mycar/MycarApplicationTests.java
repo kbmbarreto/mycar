aa7f6870e32f974ed73608e37309649f0dfe9780
cskmacedo <kleber.barreto@msn.com>
2022-10-26 19:49:11 -0300

    Realizada limpeza de código no POM.XML

--- src/test/java/com/lambdateam/mycar/MycarApplicationTests.java
+++ src/test/java/com/lambdateam/mycar/MycarApplicationTests.java
@@ -1,6 +1,5 @@
 package com.lambdateam.mycar;
 
-import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,21 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MycarApplicationTests {
 
     @Test
-    @Description("Teste exemplo 1")
     void testOperation() {
         int result = 2 + 2;
         assertEquals(4, result);
     }
 
     @Test
-    @Description("Teste exemplo 2")
     void testOperation2() {
         int result = 2 + 2;
         assertEquals(4, result);
     }
 
     @Test
-    @Description("Teste exemplo 3")
     void testOperatio3() {
         int result = 2 + 2;
         assertEquals(4, result);
