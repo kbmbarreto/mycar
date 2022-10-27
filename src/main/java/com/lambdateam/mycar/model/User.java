d5063e5d6dd591257257b457adaa57e9a44e8cb2
cskmacedo <kleber.barreto@msn.com>
2022-10-27 17:22:56 -0300

    Adicionada a classe User

--- src/main/java/com/lambdateam/mycar/model/User.java
+++ src/main/java/com/lambdateam/mycar/model/User.java
@@ -53,4 +53,29 @@ public class User implements Serializable {
     public void setPassword(String password) {
         Password = password;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if(Id == null) {
+            if(other.Id != null)
+                return false;
+        } else if(!Id.equals(other.Id))
+            return false;
+        return true;
+    }
 }
