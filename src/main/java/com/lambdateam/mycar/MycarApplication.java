f6c356a0b3e2902a4596b7e7dea6849bea929913
cskmacedo <kleber.barreto@msn.com>
2022-10-27 16:56:36 -0300

    Adicionada a classe User

--- src/main/java/com/lambdateam/mycar/MycarApplication.java
+++ src/main/java/com/lambdateam/mycar/MycarApplication.java
@@ -2,11 +2,21 @@ package com.lambdateam.mycar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.lambdateam.mycar.model"})
+@ComponentScan(basePackages = {"com.lambdateam.mycar.*"})
+@EnableJpaRepositories(basePackages = {"com.lambdateam.mycar.repository"})
+@EnableTransactionManagement
+@RestController
 public class MycarApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lambdateam/mycar/model/User.java
@@ -0,0 +1,53 @@
+package com.lambdateam.mycar.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "UserName", length = 60, columnDefinition = "VARCHAR(60)", nullable = false)
+    private String UserName;
+
+    @Column(name = "Email", length = 60, columnDefinition = "VARCHAR(60)", nullable = false)
+    private String Email;
+
+    @Column(name = "Password", length = 255, columnDefinition = "VARCHAR(255)", nullable = false)
+    private String Password;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
