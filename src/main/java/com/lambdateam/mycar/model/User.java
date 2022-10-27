d6f334cbeb3ddee42a1f10165e3ef61e3da635ad
cskmacedo <kleber.barreto@msn.com>
2022-10-27 17:02:08 -0300

    Adicionada a classe User

--- src/main/java/com/lambdateam/mycar/model/User.java
+++ src/main/java/com/lambdateam/mycar/model/User.java
@@ -1,10 +1,11 @@
 package com.lambdateam.mycar.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "User")
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
