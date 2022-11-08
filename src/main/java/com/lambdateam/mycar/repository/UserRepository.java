7a6cf75358ffbe9240c32d0513937a7ed4b759c0
cskmacedo <kleber.barreto@msn.com>
2022-11-07 23:44:19 -0300

    Implementando o spring security

--- src/main/java/com/lambdateam/mycar/repository/UserRepository.java
+++ src/main/java/com/lambdateam/mycar/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.lambdateam.mycar.repository;
 
 import com.lambdateam.mycar.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
-    
+
+    @Query("select u from UserModel u where u.username = ?1")
+    UserModel findUserByLogin(String userName);
 }
---
+++ src/main/java/com/lambdateam/mycar/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.lambdateam.mycar.service;
+
+import com.lambdateam.mycar.model.UserModel;
+import com.lambdateam.mycar.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserModel userModel = userRepository.findUserByLogin(username);
+
+        if(userModel == null) {
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return new User(userModel.getUserName(), userModel.getPassword(), userModel.getAuthorities());
+    }
+}
