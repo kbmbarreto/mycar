316a22a86d9caed447741d17cdbc11a03c370ba6
cskmacedo <kleber.barreto@msn.com>
2023-03-03 13:30:24 -0300

    Adicionado Dto de maintenances

---
+++ src/main/java/com/lambdateam/mycar/dto/MaintenancesDto.java
@@ -0,0 +1,29 @@
+package com.lambdateam.mycar.dto;
+
+import com.lambdateam.mycar.model.ComponentModel;
+import com.lambdateam.mycar.model.MaintenanceTypeModel;
+import com.lambdateam.mycar.model.ManufacturerModel;
+import com.lambdateam.mycar.model.VehicleModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+
+@Getter
+@Setter
+public class MaintenancesDto {
+
+    private long id;
+    @NotNull(message = "KM is required")
+    private double km;
+    @NotNull(message = "Maintenance date is required")
+    private Date maintenanceDate;
+    private double nextKm;
+    private double amount;
+    private ManufacturerModel manufacturer;
+    private VehicleModel vehicle;
+    private ComponentModel component;
+    private MaintenanceTypeModel maintenanceType;
+}
