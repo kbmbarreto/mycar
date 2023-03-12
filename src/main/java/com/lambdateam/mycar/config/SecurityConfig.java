d29d81a67f89dfe9668bbf86af2f5ed0a06ca3b3
cskmacedo <kleber.barreto@msn.com>
2023-03-12 12:09:32 -0300

    Adicionadas as politicas de CORS

---
+++ src/main/java/com/lambdateam/mycar/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+//package com.lambdateam.mycar.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//    }
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//}
